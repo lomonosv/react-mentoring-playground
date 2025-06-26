a22a1e95cdefabba067b7c4f04ebf8f6cdefaf1e
Vladimir Lomonos <vladimir.lomonos@finsight.com>
2025-06-26 11:21:30 +0300

    Cleanup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Home from "../src/Home";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +8,6 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function HomeRoute() {
+  return <Home />;
 }
